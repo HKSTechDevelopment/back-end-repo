9c63fe54867ff4b7967b9337f5f3fbb5cec4bfe3
DANIELA <daniela.serrano.p@gmail.com>
2017-01-15 20:28:11 -0500

    changed data format fto json

--- water_chart/data.js
+++
@@ -1,7 +0,0 @@
-var data = {
-	"marine" : { "past": 5.08, "present": 12.18 }
-	"terrestial" : { "past": 8.21, "present": 14.81 }
-	"electricity" : { "past": 75.65, "present": 84.58 }
-	"forest" : { "past": 41282694.8, "present": 39991336.2 }
-	"CO2" : { "past": 9385792.843, "present": 35848592 }
-}
